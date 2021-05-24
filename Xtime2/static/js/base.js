653ddb5de5b5a1eea882a0d6319828eb3b356292
Franktjp <1115303890@qq.com>
2021-05-24 16:49:15 +0800

    base.html引用toast插件，将jquery.js文件和toasts.js文件放在head标签中

--- Xtime2/static/js/base.js
+++ Xtime2/static/js/base.js
@@ -1,17 +1,34 @@
- $(document).ready(function() {
-            $(".menu-icon").on("click", function() {
-                  $("nav ul").toggleClass("showing");
-            });
-      });
+$(document).ready(function () {
+    $(".menu-icon").on("click", function () {
+        $("nav ul").toggleClass("showing");
+    });
+});
 
-      // Scrolling Effect
+// Scrolling Effect
+$(window).on("scroll", function () {
+    if ($(window).scrollTop()) {
+        $('nav').addClass('black');
+    } else {
+        $('nav').removeClass('black');
+    }
+})
 
-      $(window).on("scroll", function() {
-            if($(window).scrollTop()) {
-                  $('nav').addClass('black');
-            }
-
-            else {
-                  $('nav').removeClass('black');
-            }
-      })
+// toatsr插件配置
+$(function () {
+    // 设置显示配置
+    var messageOpts = {
+        "closeButton": true,//是否显示关闭按钮
+        "debug": false,//是否使用debug模式
+        "positionClass": "toast-top-left",//弹出窗的位置(注：与预期不符，会显示在右上角)
+        "onclick": null,
+        "showDuration": "500",//显示的动画时间
+        "hideDuration": "1000",//消失的动画时间
+        "timeOut": "3000",//展现时间
+        "extendedTimeOut": "1000",//加长展示时间
+        "showEasing": "swing",//显示时的动画缓冲方式
+        "hideEasing": "linear",//消失时的动画缓冲方式
+        "showMethod": "fadeIn",//显示时的动画方式
+        "hideMethod": "fadeOut" //消失时的动画方式
+    };
+    toastr.options = messageOpts;
+})
